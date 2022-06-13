f89cb68c4fa72d2e398bafcc861c339d2752a239
Alan Junqueira <contato.alanjunqueira@gmail.com>
2022-06-13 07:13:03 -0300

    Js Hover E-mail and Password Need help with checkbox Input

--- script.js
+++ script.js
@@ -1,16 +1,103 @@
-const emailDefault = document.querySelector('.e-mail')
-const passwordDefault = document.querySelector('.password')
-const emailHover = document.querySelector('.emailFocus')
-const passwordHover = document.querySelector('.passwordFocus')
+function activeHoverEmail() {
+  const emailHover = document.querySelector('.emailFocus')
+  const emailDefault = document.querySelector('.e-mail div')
+  const label = document.querySelector('.e-mail div label')
+  const img = document.querySelector('.e-mail div img')
+  const input = document.querySelector('.e-mail div input')
 
-function defaultMenu() {
-  if (emailDefault.classList.value && passwordDefault.classList.value) {
-    emailDefault.classList.add('emailAtivo')
-    passwordDefault.classList.add('passwordAtivo')
+  function activeHoverEmail() {
+    emailHover.classList.add('emailFocusAtivo')
+    emailDefault.classList.add('emailHidden')
+    label.classList.add('emailHidden')
+    img.classList.add('emailHidden')
+    input.classList.add('emailHidden')
   }
-}
-defaultMenu()
 
-function removeDefaultMenu() {
-  forEach()
+  function removeDefaultMenuEmail() {
+    if (emailDefault.classList.length === 0) {
+      emailDefault.addEventListener('mouseenter', activeHoverEmail)
+    }
+  }
+  removeDefaultMenuEmail()
 }
+activeHoverEmail()
+
+function unActiveHoverEmail() {
+  const emailHover = document.querySelector('.emailFocus')
+  const emailDefault = document.querySelector('.e-mail div')
+  const label = document.querySelector('.e-mail div label')
+  const img = document.querySelector('.e-mail div img')
+  const input = document.querySelector('.e-mail div input')
+  const pEmailFocus = document.querySelector('.emailFocus p')
+  const section = document.querySelector('section')
+  const pPasswordFocus = document.querySelector('.password p')
+
+  function activeHoverEmail() {
+    emailHover.classList.remove('emailFocusAtivo')
+    emailDefault.classList.remove('emailHidden')
+    label.classList.remove('emailHidden')
+    img.classList.remove('emailHidden')
+    input.classList.remove('emailHidden')
+  }
+
+  function removeDefaultMenuEmail() {
+    if (section.classList.length === 1) {
+      pEmailFocus.addEventListener('mouseenter', activeHoverEmail),
+        pPasswordFocus.addEventListener('mouseenter', activeHoverEmail)
+    }
+  }
+  removeDefaultMenuEmail()
+}
+unActiveHoverEmail()
+
+function activeHoverPassword() {
+  const passwordHover = document.querySelector('.passwordFocus')
+  const passwordDefault = document.querySelector('.password div')
+  const label = document.querySelector('.password div label')
+  const img = document.querySelector('.password div img')
+  const input = document.querySelector('.password div input')
+
+  function activeHoverpassword() {
+    passwordHover.classList.add('passwordFocusAtivo')
+    passwordDefault.classList.add('passwordHidden')
+    label.classList.add('passwordHidden')
+    img.classList.add('passwordHidden')
+    input.classList.add('passwordHidden')
+  }
+
+  function removeDefaultMenuPassword() {
+    if (passwordDefault.classList.length === 0) {
+      passwordDefault.addEventListener('mouseenter', activeHoverpassword)
+    }
+  }
+  removeDefaultMenuPassword()
+}
+activeHoverPassword()
+
+function unActiveHoverPassword() {
+  const passwordHover = document.querySelector('.passwordFocus')
+  const passwordDefault = document.querySelector('.password div')
+  const label = document.querySelector('.password div label')
+  const img = document.querySelector('.password div img')
+  const input = document.querySelector('.password div input')
+  const pPasswordFocus = document.querySelector('.passwordFocus p')
+  const section = document.querySelector('section')
+  const checkbox = document.querySelector('.checkbox')
+
+  function activeHoverPassword() {
+    passwordHover.classList.remove('passwordFocusAtivo')
+    passwordDefault.classList.remove('passwordHidden')
+    label.classList.remove('passwordHidden')
+    img.classList.remove('passwordHidden')
+    input.classList.remove('passwordHidden')
+  }
+
+  function removeDefaultMenuPassword() {
+    if (section.classList.length === 1) {
+      pPasswordFocus.addEventListener('mouseenter', activeHoverPassword),
+      checkbox.addEventListener('mouseenter', activeHoverPassword)
+    }
+  }
+  removeDefaultMenuPassword()
+}
+unActiveHoverPassword()
